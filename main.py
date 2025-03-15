861587de5c20777b8220db52d339e6b3ead13720
N3dS3 <121673558+N3dS3@users.noreply.github.com>
2025-03-15 02:15:42 -0400

    base code

--- main.py
+++ main.py
@@ -40,4 +40,38 @@ prompt = tokenizer.apply_chat_template(
 
 
 def contrastive_generation(amateur, expert, prompt, max_tokens) -> str:
-    return ""
+    '''Implements https://arxiv.org/abs/2210.15097'''
+
+    # Tokenize the prompt
+	prompt = tokenizer(prompt)["input_ids"]
+
+	# Use GPU if available
+	device = torch.device(
+		"mps" if torch.backends.mps.is_available() # (I'm coding on a Mac w/ an M1 chip)
+		else "cuda" if torch.cuda.is_available() # (I tested using Google Colab)
+		else "cpu"
+	)
+	amateur = amateur.to(device)
+	expert = expert.to(device)
+
+	# Generate response
+	response = []
+	for _ in range(max_tokens):
+		print(_)
+		# Get amateur logits and expert logits
+		input_tokens = torch.tensor([prompt + response]).to(device) # (1, input_length)
+		amateur_logits = amateur(input_tokens).logits[0,-1,:]
+		expert_logits = expert(input_tokens).logits[0,-1,:]
+		
+		# Get the next token using CD objective
+		next_token = torch.argmax(expert_logits - amateur_logits).item()
+		if next_token == tokenizer.eos_token_id:
+			break
+		response.append(next_token)
+
+	return tokenizer.decode(response)
+
+amateur = tr.AutoModelForCausalLM.from_pretrained(amateur_path)
+expert = tr.AutoModelForCausalLM.from_pretrained(expert_path)
+response = contrastive_generation(amateur, expert, prompt, 100)
+print(response)
